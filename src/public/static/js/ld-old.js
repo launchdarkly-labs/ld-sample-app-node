ae23f10e06405a6d9c90c4d6fa792b072c5b340e
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2026-03-13 14:10:48 -0400

    update with esbuild

---
+++ src/public/static/js/ld-old.js
@@ -0,0 +1,132 @@
+const contexts = {
+    "anonymous":
+    {
+        "kind": "user",
+        "key": "user-018e7bd4-ab96-782e-87b0-b1e32082b481",
+        "anonymous": true
+    },
+    "developer":
+    {
+        "kind": "multi",
+        "user": {
+            "key": "user-018e7bd4-ab96-782e-87b0-b1e32082b481",
+            "name": "Miriam Wilson",
+            "language": "en",
+            "tier": "premium",
+            "userId": "mwilson",
+            "role": "developer",
+            "email": "miriam.wilson@example.com",
+        },
+        "device": {
+            "key": "device-018e7bd4-ab96-782e-87b0-b1e32082b481",
+            "os": "macOS",
+            "osVersion": "15.6",
+            "model": "MacBook Pro",
+            "manufacturer": "Apple",
+        },
+    },
+    "betauser":
+    {
+        "kind": "multi",
+        "user": {
+            "key": "user-018e7bd4-ab96-782e-87b0-b1e32082b481",
+            "name": "Alex Bailey",
+            "language": "en",
+            "tier": "standard",
+            "userId": "abailey",
+            "role": "betauser",
+            "email": "alex.bailey@example.com",
+        },
+        "device": {
+            "key": "device-018e7bd4-ab96-782e-87b0-b1e32082b481",
+            "os": "Android",
+            "osVersion": "15",
+            "model": "Galaxy S25 Ultra",
+            "manufacturer": "Samsung",
+        },
+    },
+    "customer":
+    {
+        "kind": "multi",
+        "user": {
+            "key": "user-018e7bd4-ab96-782e-87b0-b1e32082b481",
+            "name": "Wayne Lancaster",
+            "language": "en",
+            "tier": "member",
+            "userId": "wlancaster",
+            "role": "customer",
+            "email": "wayne.lancaster@example.com",
+        },
+        "device": {
+            "key": "device-018e7bd4-ab96-782e-87b0-b1e32082b481",
+            "os": "Android",
+            "osVersion": "15",
+            "model": "Pixel 10 Pro XL",
+            "manufacturer": "Google",
+        },
+    }
+};
+
+const client = LDClient.initialize(clientKey, contexts.anonymous);
+
+client.on('ready', () => {
+    // initialization succeeded, flag values are now available
+    const flagValue = client.variation('release-home-page-slider', false);
+    // etc.
+});
+
+client.on('change', (settings) => {
+    console.log(settings);
+    if (settings['release-home-page-slider']) {
+        switchSlider(settings['release-home-page-slider'].current);
+    }
+    if (settings['coffee-promo-1']) {
+        switchPromo(settings['coffee-promo-1'].current, 1);
+    }
+    if (settings['coffee-promo-2']) {
+        switchPromo(settings['coffee-promo-2'].current, 2);
+    }
+    switchSection();
+})
+
+function switchSlider(enabled) {
+    slider = document.getElementById('home-page-slider');
+    if (enabled) {
+        slider.style.display = 'block';
+    } else {
+        slider.style.display = 'none';
+    }
+}
+
+function switchPromo(enabled, id) {
+    promo = document.getElementById('coffee-promo-' + id);
+    if (enabled) {
+        promo.style.display = 'block';
+    } else {
+        promo.style.display = 'none';
+    }
+}
+
+function switchSection() {
+    section = document.getElementById('coffee-promo-section');
+    promo1 = document.getElementById('coffee-promo-1');
+    promo2 = document.getElementById('coffee-promo-2');
+    if (promo1.style.display !== 'none' || promo2.style.display !== 'none') {
+        section.style.display = 'block';
+    } else {
+        section.style.display = 'none';
+    }
+}
+
+function getBannerText() {
+    fetch('/api/banner')
+        .then(response => response.json())
+        .then(data => {
+            const bannerText = data.primaryBanner;
+            const heroBanner = document.getElementById('hero-banner');
+            heroBanner.textContent = bannerText;
+        })
+        .catch(error => {
+            console.error('Error fetching banner text:', error);
+        });
+}
--- src/public/static/js/ld.js
+++ src/public/static/js/ld.js
@@ -1,83 +1,125 @@
-const context = {
-    "kind": "multi",
-    "user": {
-        "key": "user-018e7bd4-ab96-782e-87b0-b1e32082b481",
-        "name": "Miriam Wilson",
-        "language": "en",
-        "tier": "premium",
-        "userId": "mwilson",
-        "role": "developer",
-        "email": "miriam.wilson@example.com",
-    },
-    "device": {
-        "key": "device-018e7bd4-ab96-782e-87b0-b1e32082b481",
-        "os": "macOS",
-        "osVersion": "15.6",
-        "model": "MacBook Pro",
-        "manufacturer": "Apple",
-    },
-};
+import { createClient } from '@launchdarkly/js-client-sdk';
+import Observability from '@launchdarkly/observability';
+import SessionReplay from '@launchdarkly/session-replay';
+// import { useLaunchDarklyToolbar, FlagOverridePlugin, EventInterceptionPlugin } from '@launchdarkly/toolbar';
 
-const client = LDClient.initialize(clientKey, context);
 
-client.on('ready', () => {
-    // initialization succeeded, flag values are now available
-    const flagValue = client.variation('release-home-page-slider', false);
-    // etc.
-});
+class PageHandler {
+    constructor() {
+        this.ldclient = null;
+        this.context = this.getContext();
+        this.homeSlider = null;
+        this.coffeePromo1 = null;
+        this.coffeePromo2 = null;
+        document.addEventListener('DOMContentLoaded', this.onPageLoad.bind(this));
 
-client.on('change', (settings) => {
-    console.log(settings);
-    if (settings['release-home-page-slider']) {
-        switchSlider(settings['release-home-page-slider'].current);
+        this.init();
     }
-    if (settings['coffee-promo-1']) {
-        switchPromo(settings['coffee-promo-1'].current, 1);
-    }
-    if (settings['coffee-promo-2']) {
-        switchPromo(settings['coffee-promo-2'].current, 2);
-    }
-    switchSection();
-})
 
-function switchSlider(enabled) {
-    slider = document.getElementById('home-page-slider');
-    if (enabled) {
-        slider.style.display = 'block';
-    } else {
-        slider.style.display = 'none';
-    }
-}
-
-function switchPromo(enabled, id) {
-    promo = document.getElementById('coffee-promo-' + id);
-    if (enabled) {
-        promo.style.display = 'block';
-    } else {
-        promo.style.display = 'none';
-    }
-}
-
-function switchSection() {
-    section = document.getElementById('coffee-promo-section');
-    promo1 = document.getElementById('coffee-promo-1');
-    promo2 = document.getElementById('coffee-promo-2');
-    if (promo1.style.display !== 'none' || promo2.style.display !== 'none') {
-        section.style.display = 'block';
-    } else {
-        section.style.display = 'none';
-    }
-}
-
-function getBannerText() {
-    fetch('/api/banner')
-        .then(response => response.json())
-        .then(data => {
-            const bannerText = data.primaryBanner;
-            const heroBanner = document.getElementById('hero-banner');
-            heroBanner.textContent = bannerText;
-        })
-        .catch(error => {
-            console.error('Error fetching banner text:', error);
+    onPageLoad() {
+        document.getElementById("page-banner-text").addEventListener("click", () => {
+            this.getBannerText();
         });
+    }
+
+    init() {
+        const client = createClient(clientKey, this.context, {
+            plugins: [
+                new Observability(),
+                new SessionReplay()
+            ],
+            streaming: true
+        });
+        client.start();
+        this.ldclient = client;
+
+        this.ldclient.on('ready', () => {
+            // initialization succeeded, flag values are now available
+            this.homeSlider = this.ldclient.variation('release-home-page-slider', false);
+            this.coffeePromo1 = this.ldclient.variation('coffee-promo-1', false);
+            this.coffeePromo2 = this.ldclient.variation('coffee-promo-2', false);
+            // etc.
+        });
+
+        this.ldclient.on('change:release-home-page-slider', (context) => {
+            this.switchSlider(this.ldclient.variation('release-home-page-slider', false));
+        });
+
+        this.ldclient.on('change:coffee-promo-1', (context) => {
+            this.switchPromo(this.ldclient.variation('coffee-promo-1', false), 1);
+        });
+
+        this.ldclient.on('change:coffee-promo-2', (context) => {
+            this.switchPromo(this.ldclient.variation('coffee-promo-2', false), 2);
+        });
+    }
+
+    getContext() {
+        return {
+            "kind": "multi",
+            "user": {
+                "key": "user-018e7bd4-ab96-782e-87b0-b1e32082b481",
+                "name": "Miriam Wilson",
+                "language": "en",
+                "tier": "premium",
+                "userId": "mwilson",
+                "role": "developer",
+                "email": "miriam.wilson@example.com",
+            },
+            "device": {
+                "key": "device-018e7bd4-ab96-782e-87b0-b1e32082b481",
+                "os": "macOS",
+                "osVersion": "15.6",
+                "model": "MacBook Pro",
+                "manufacturer": "Apple",
+            },
+        };
+    }
+
+    switchSlider(enabled) {
+        const slider = document.getElementById('home-page-slider');
+        if (enabled) {
+            slider.style.display = 'block';
+        } else {
+            slider.style.display = 'none';
+        }
+    }
+
+    switchPromo(enabled, id) {
+        const promo = document.getElementById('coffee-promo-' + id);
+        if (enabled) {
+            promo.style.display = 'block';
+        } else {
+            promo.style.display = 'none';
+        }
+    }
+
+    switchSection() {
+        const section = document.getElementById('coffee-promo-section');
+        const promo1 = document.getElementById('coffee-promo-1');
+        const promo2 = document.getElementById('coffee-promo-2');
+        if (promo1.style.display !== 'none' || promo2.style.display !== 'none') {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    }
+
+    getBannerText() {
+        fetch('/api/banner')
+            .then(response => response.json())
+            .then(data => {
+                const bannerText = data.primaryBanner;
+                const heroBanner = document.getElementById('hero-banner');
+                heroBanner.textContent = bannerText;
+                console.log('Banner text updated:', bannerText);
+            })
+            .catch(error => {
+                console.error('Error fetching banner text:', error);
+            });
+    }
 }
+
+const pageHandler = new PageHandler();
+
+export default pageHandler;
